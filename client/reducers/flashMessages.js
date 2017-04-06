c806aa97e84f12172e56a44fdb3172f5282d8ea2
rajasyed <rajasyedab@gmail.com>
2017-04-06 09:32:01 +0530

    flash message rendering

--- client/reducers/flashMessages.js
+++ client/reducers/flashMessages.js
@@ -1,4 +1,6 @@
-import { ADD_FLASH_MESSAGE } from '../actions/types';
+import { ADD_FLASH_MESSAGE, DELETE_FLASH_MESSAGE } from '../actions/types';
+
+import findIndex from 'lodash/findIndex';
 import shortid from 'shortid';
 
 export default (state = [], action = {}) => {
@@ -12,6 +14,15 @@ export default (state = [], action = {}) => {
           text: action.message.text
         }
       ];
+    case DELETE_FLASH_MESSAGE:
+      const index = findIndex(state,{ id: action.id})
+      if(index >=0){
+        return [
+          ...state.slice(0,index),
+          ...state.slice(index+1)
+        ];
+      }
+      return state;
     default: return state;
   }
 }
